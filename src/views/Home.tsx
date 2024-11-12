8f5931d26103223b8ffea682dde3572dc15dce97
SeongYun_Cheon <1112csy@naver.com>
2024-11-12 16:44:16 +0900

    init: package install

---
+++ src/views/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='page'>
+        <div className='page__container'>
+            <header>
+                헤더
+            </header>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Home
