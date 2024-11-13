7c0b0af962405c1eee0953e7e2feaf4c50921852
SeongYun_Cheon <1112csy@naver.com>
2024-11-13 09:45:58 +0900

    INIT: scss fonts and color

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
 function Home() {
-  return (
+  return (  
     <div className='page'>
         <div className='page__container'>
-            <header className='w-full h-20 flex'>
-                헤더
+            <header className='w-full h-20 flex items-center p-6 gap-6 bg-slate-800'>
+            <div className='w-1/2 flex items-center justify-start gap-6'>
+            {/*로고영역 */}
+            <div className='h-full flex items-center justify-center gap-2'>
+              {/*아이콘영역 */}
+              <img src='' alt='logo' className='h-10'></img>
+              {/*폰트 로고 영역 */}
+              <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                  The Joke Tax
+                </h3>
+            </div>
+            <div>
+              {/*검색창영역 */}
+              </div>
+            </div>
             </header>
         </div>
       
